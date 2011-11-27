077237220196b5f5179cc2542d9663211f705e15
Luke Lowrey <lukenlowrey@gmail.com>
2011-11-27 21:07:20 +1000

    made it a little easier to add logs

--- logR.Demo/Controllers/HomeController.cs
+++ logR.Demo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using logR.Demo.Models;
 
 namespace logR.Demo.Controllers
 {
@@ -12,8 +13,31 @@ namespace logR.Demo.Controllers
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-            LogR.Log("HomeController", Level.Error, "Oh noes error!");
-            return View();
+            var model = new HomeViewModel();
+            model.Levels = new List<SelectListItem>()
+                {
+                    new SelectListItem() { Text = Level.Trace.ToString(), Value = ((int)Level.Trace).ToString() },
+                    new SelectListItem() { Text = Level.Debug.ToString(), Value = ((int)Level.Debug).ToString() },
+                    new SelectListItem() { Text = Level.Info.ToString(), Value = ((int)Level.Info).ToString() },
+                    new SelectListItem() { Text = Level.Warn.ToString(), Value = ((int)Level.Warn).ToString() },
+                    new SelectListItem() { Text = Level.Error.ToString(), Value = ((int)Level.Error).ToString() },
+                    new SelectListItem() { Text = Level.Fatal.ToString(), Value = ((int)Level.Fatal).ToString() }
+                };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HomeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                LogR.Log("HomeController", (Level)Convert.ToInt32(model.SelectedLevel), model.Message);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ logR.Demo/Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace logR.Demo.Models
+{
+    public class HomeViewModel
+    {
+        public string Message { get; set; }
+        public string SelectedLevel { get; set; }
+
+        public List<SelectListItem> Levels { get; set; }
+    }
+}
